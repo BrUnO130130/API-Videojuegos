6fde1c34c44850bb5e3f640972e2f24e050172b3
Bruno <xxxnikgamexxx@gmail.com>
2026-06-08 23:59:57 -0300

    probando creacion de base de datos

--- CatalogoVideojuegos/Program.cs
+++ CatalogoVideojuegos/Program.cs
@@ -1,41 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Data.Sqlite;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+string rutaBaseDeDatos = "videojuegos.db";
+using var conexion = new SqliteConnection($"Data Source={rutaBaseDeDatos}");
+conexion.Open();
 
-var app = builder.Build();
+var crearTabla = conexion.CreateCommand();
+crearTabla.CommandText = @"
+CREATE TABLE IF NOT EXISTS Videojuegos (
+Id INTEGER PRIMARY KEY AUTOINCREMENT,
+Nombre TEXT NOT NULL,
+Genero TEXT NOT NULL,
+Precio REAL NOT NULL,
+Añolanzamiento INTEGER NOT NULL,
+    FOREIGN KEY (IdDesarrollador)
+        REFERENCES Desarrolladores(Id)
+)
+";
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+crearTabla.ExecuteNonQuery();
